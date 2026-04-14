cf1a2fdf4a24dacfc0ac1f44283e05f9f528e4cd
craftluis12 <88074367+craftluis12@users.noreply.github.com>
2026-04-13 22:43:46 -0400

    Add files via upload

---
+++ ssh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# -------------------------------
+# SSH setup
+# -------------------------------
+sudo apt install -y openssh-server
+sudo systemctl enable ssh
+sudo systemctl start ssh
+
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^#\?KbdInteractiveAuthentication.*/KbdInteractiveAuthentication yes/' /etc/ssh/sshd_config
+
+sudo sshd -t && sudo systemctl restart ssh
